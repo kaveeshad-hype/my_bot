e0f33e97f814339fe9cb2d9443ccf63ab17497ad
kaveeshad-hype <kaveesha.d@hypeinvention.com>
2025-09-28 22:06:37 +0530

    Adding ros2_control and cleaning up the code

--- launch/launchusim.launch.py
+++ launch/launchusim.launch.py
@@ -4,11 +4,10 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-
 
 
 def generate_launch_description():
@@ -34,7 +33,18 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+    
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
 
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
 
 
     # Launch them all!
@@ -42,4 +52,11 @@ def generate_launch_description():
         rsp,
         gazebo,
         spawn_entity,
+        TimerAction(
+            period=5.0,
+                actions=[
+                    diff_drive_spawner,
+                    joint_broad_spawner,
+                ]
+    )
     ])
